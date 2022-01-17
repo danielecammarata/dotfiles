48cb706eb8a361287a976f95682a355706014615
Daniele Cammarata <daniele.cammarata@eprice.it>
2022-01-17 16:25:50 +0100

    initial script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dotfiles
 My dotfiles and personal preferences
+
+## Install Script
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/danielecammarata/dotfiles/main/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# inspired by
+# https://gist.github.com/codeinthehole/26b37efa67041e1307db
+# https://github.com/why-jay/osx-init/blob/master/install.sh
+# https://github.com/timsutton/osx-vm-templates/blob/master/scripts/xcode-cli-tools.sh
+
+# PRECONDITIONS
+# 1)
+# make sure the file is executable
+# chmod +x osx_bootstrap.sh
+#
+# 2)
+# Your password may be necessary for some packages
+#
+# 3)
+# https://docs.brew.sh/Installation#macos-requirements
+# xcode-select --install
+# (_xcode-select installation_ installs git already, however git will be installed via brew packages as well to install as much as possible the brew way
+#  this way you benefit from frequent brew updates)
+# 
+# 4) don't let the “Operation not permitted” error bite you
+# Please make sure you system settings allow the termianl full disk access
+# https://osxdaily.com/2018/10/09/fix-operation-not-permitted-terminal-error-macos/
+
+# `set -eu` causes an 'unbound variable' error in case SUDO_USER is not set
+SUDO_USER=$(whoami)
+
+# Check for Homebrew, install if not installed
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+brew update
+brew upgrade
+
+# find the CLI Tools update
+echo "find CLI tools update"
+PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n') || true
+# install it
+if [[ ! -z "$PROD" ]]; then
+  softwareupdate -i "$PROD" --verbose
+fi
+
+# alfred
+# tmux
+# rectangle
+
+PACKAGES=(
+    ack
+    autojump
+    bash
+    fnm
+    gcc
+    git
+    httpie
+    hub
+    imagemagick
+    lynx
+    markdown
+    node
+    rename
+    ripgrep
+    the_silver_searcher
+    tig
+    tree
+    watch
+    wget
+    yarn
+    zsh
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+CASKS=(
+    chromium
+    firefox
+    google-chrome
+    keepassxc
+    keepingyouawake
+    slack
+    visual-studio-code
+    vlc
+    whatsapp
+    zoom
+)
+
+echo "Installing cask apps..."
+sudo -u $SUDO_USER brew install --cask ${CASKS[@]}
+
+echo "Installing docker..."
+sudo -u $SUDO_USER brew install --cask docker
+
+echo "Installing oh-my-zsh..."
+/bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "Cleaning up"
+brew cleanup
+echo "Ask the doctor"
+brew doctor
+
+echo "OSX bootstrapping done"
